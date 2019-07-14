1e8c3330403fcbbf28d4525ed4c4b66a71bee8ff
Hendrick Brian B. Queza <52022216+hendrickbrianb@users.noreply.github.com>
2019-07-14 18:36:12 +0800

    Update Problem 6 Experiment 2.cpp

--- Problem 6 Experiment 2.cpp
+++ Problem 6 Experiment 2.cpp
@@ -1,4 +1,3 @@
-
 #include <iostream>
 #include <conio.h>
 using namespace std;
@@ -8,17 +7,26 @@ int main ()
   int n, sum = 0;
     int x;
   do {
-   cout <<"\n"<< "Enter a Number: "<<" ";
+  	cout <<endl;
+   	cout << "Enter a Number: "<<" ";
     cin >> n;
+    if (n>0){
+	
     for (int i = 1; i <= n; ++i) 
+    
      sum += i;
     cout << "The sum of all whole numbers from 1 to "<< n << " is " << sum<<".";
-  } 
-  while (n != 0);
-  {
-  	cout <<"\n---------------------------------------------"<< endl;
-  	cout <<"\nThank You" << endl;
-  }
+
+}
+    else
+    {
+	
+    cout << "Thank You!"<< endl;
+    
+	}
+  } while (n != 0);
+ 
   getch();
   return 0;
 }
+
