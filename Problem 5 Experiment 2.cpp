943befb23ab883da9251bb86bd2872d4862a6a21
Hendrick Brian B. Queza <52022216+hendrickbrianb@users.noreply.github.com>
2019-07-14 18:01:55 +0800

    Update Problem 5 Experiment 2.cpp

--- Problem 5 Experiment 2.cpp
+++ Problem 5 Experiment 2.cpp
@@ -11,19 +11,18 @@ int main()
 	num1 = 0;
 	num2 = 1;
 	ffterm = 0;
-    cout << "Type How Many Terms: ";
-    cin >> fibonacci_term;
-    cout << "Fibonacci Number Series: ";
-    for (fibonacci_number = 1; fibonacci_number <= fibonacci_term; ++fibonacci_number)
+
+    cout << "Fibonacci Numbers: ";
+    for (fibonacci_number = 1; fibonacci_number <= 22; ++fibonacci_number)
     {
         if(fibonacci_number == 1)
         {
-            cout << " " << num1;
+            cout <<"\n"<< num1 <<",";
             continue;
         }
         if(fibonacci_number == 2)
         {
-            cout << num2 << " ";
+            cout << num2 << ",";
             continue;
         }
         ffterm = num1 + num2;
