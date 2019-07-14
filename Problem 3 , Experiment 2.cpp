e3166f2c5eff118b44ccca4102a4a416589d4ab0
Hendrick Brian B. Queza <52022216+hendrickbrianb@users.noreply.github.com>
2019-07-14 17:42:32 +0800

    Update Problem 3 , Experiment 2.cpp

--- Problem 3 , Experiment 2.cpp
+++ Problem 3 , Experiment 2.cpp
@@ -1,25 +1,35 @@
 #include<iostream>
 #include<conio.h>
 #include<math.h>
+
 using namespace std;
+
 int main()
 
 {
+	
 double v1,v2,v4,v5;
 float v3;
 short int x,x1,x2,x3,x4;
 int y;
-float z = 2.5;
-cout << "Enter Your Choice of x" << endl;
+const float z = 2.5;
+
+cout << "Enter Your Choice of x:"<<" "<<flush;
 cin >> x;
-cout << "Enter Your Choice of y" << endl;
+cout << endl;
+	
+cout << "Enter Your Choice of y:"<<" "<<flush;
 cin >> y;
+cout << endl;
 
 if (x>=1 && x<=2){
 
+// switch control statement is used to seclect the value of x.
+
 switch (x){
 		case 1:
-				if (y>=1 && y<=4){
+		// in each statement there is a y condition. If the values if between the condition the equation will evaluated.
+			if (y>=1 && y<=4){
 				x1 = 1;
 				v1 = x1 * y * z;
 				cout <<"Answer is: "<< v1;
@@ -32,23 +42,25 @@ switch (x){
 				cout << "Invalid" << endl;
 				}break;
 		case 2:
-				if (y>=1 && y<=5){
-					x3 = 2;
-					v3= x3 - y / z;
-					cout <<"Answer is: "<< v3 << endl;
-				}else if(y>=6){
+			if (y>=1 && y<=5){
+				x3 = 2;
+				v3= x3 - y / z;
+				cout <<"Answer is: "<< v3 << endl;
+			}else if(y>=6){
 				x4 = 2;
 				v4 = x4 - sqrt (y+z);
 				cout <<"Answer is: " << v4 << endl;
-				}else{
-					cout <<"Invalid" << endl;
+			}else{
+				cout <<"Invalid" << endl;
 				}break;
-			}
+
+
+		}
 		}
 				else if(x>=3){
 				v5 = x + y + z;
 				cout << "Answer is: " << v5 << endl;
-			}
-	getch();
-	return 0;
+		}
+getch();
+return 0;
 }
