a23a86f8535fbb882dfa437ab0874ffc3130e638
Hendrick Brian B. Queza <52022216+hendrickbrianb@users.noreply.github.com>
2019-07-14 17:18:40 +0800

    Update Problem 2 Experiment 2.cpp

--- Problem 2 Experiment 2.cpp
+++ Problem 2 Experiment 2.cpp
@@ -4,27 +4,30 @@ using namespace std;
 int main()
 
 {	
-	short int water_demand = 35 ;
-	int special_demand = 20;
-	float additional_charge = 1.10;
+	const int water_demand = 35;
+	const int special_demand = 20;
+	const float additional_charge = 1.10;
 	float waterbill1,waterbill2,waterbill3;
 	int unpaid_balance;
 	int gallons;
 	
-	
-	cout << "Enter how many gallons monthly: " << " ";
+	cout <<"\t(Waterbill Computer)"<<endl;
+	cout <<endl;
+	cout << "(0)Enter how many gallons monthly: " << " ";
 	cin >> gallons;
-	cout << "How much is your unpaid balance: "<< " ";
+	cout<< endl;
+	cout << "(0)How much is your unpaid balance: "<< " ";
 	cin >> unpaid_balance;
+	cout <<endl;
 	if (unpaid_balance>=1){
 	waterbill1 = gallons * additional_charge + unpaid_balance + water_demand;
 	waterbill2 = 	waterbill1 + 20;
-	cout << "Your waterbill is: " << waterbill2 << endl;
+	cout << "(0)Your waterbill is: " <<"P "<< waterbill2 << endl;
 	}
 	else 
 	{
 	waterbill3 = gallons * additional_charge + unpaid_balance + water_demand;
-	cout << "Waterbill is: " << waterbill3 << endl;
+	cout << "(0)Waterbill is: " <<"P "<< waterbill3 << endl;
 	}
 
 	getch();
