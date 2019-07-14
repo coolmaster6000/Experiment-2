94132041804487b77a2c7462496ca5ce2a320ea3
Hendrick Brian B. Queza <52022216+hendrickbrianb@users.noreply.github.com>
2019-07-14 17:22:24 +0800

    Update Problem 2 Experiment 2.cpp

--- Problem 2 Experiment 2.cpp
+++ Problem 2 Experiment 2.cpp
@@ -4,6 +4,7 @@ using namespace std;
 int main()
 
 {	
+	// constant int declaration is used to assign values and stores only a number values.
 	const int water_demand = 35;
 	const int special_demand = 20;
 	const float additional_charge = 1.10;
@@ -20,12 +21,16 @@ int main()
 	cin >> unpaid_balance;
 	cout <<endl;
 	if (unpaid_balance>=1){
+	// formula to get the waterbill. waterbill that has a unpaid balance greater than zero are added to the total bill.	
 	waterbill1 = gallons * additional_charge + unpaid_balance + water_demand;
 	waterbill2 = 	waterbill1 + 20;
 	cout << "(0)Your waterbill is: " <<"P "<< waterbill2 << endl;
 	}
+	
 	else 
+		
 	{
+	// this is the same as first but there is no additional twenty if unpaid balance is less than zero.
 	waterbill3 = gallons * additional_charge + unpaid_balance + water_demand;
 	cout << "(0)Waterbill is: " <<"P "<< waterbill3 << endl;
 	}
